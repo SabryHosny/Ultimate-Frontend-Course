2a1e12ee9351d70c53ed23d78b2e118901c8a3ef
SabryHosny <sabry3hosny666@gmail.com>
2021-06-16 08:44:31 +0200

    Lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-$(function (callback) {
+$(function () {
    "use strict";
 
    // Toggle Sidebar
@@ -11,4 +11,43 @@ $(function (callback) {
       $(this).find(".fa-angle-right").toggleClass("down");
       $(this).next(".child-links").slideToggle();
    });
+
+   // Toggle Fullscreen
+   $(".toggle-fullscreen").on("click", function () {
+      $(this).toggleClass("fullscreen");
+      if ($(this).hasClass("fullscreen")) {
+         openFullscreen();
+      } else {
+         closeFullscreen();
+      }
+   });
 });
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+   if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+   } else if (elem.webkitRequestFullscreen) {
+      /* Safari */
+      elem.webkitRequestFullscreen();
+   } else if (elem.msRequestFullscreen) {
+      /* IE11 */
+      elem.msRequestFullscreen();
+   }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+   if (document.exitFullscreen) {
+      document.exitFullscreen();
+   } else if (document.webkitExitFullscreen) {
+      /* Safari */
+      document.webkitExitFullscreen();
+   } else if (document.msExitFullscreen) {
+      /* IE11 */
+      document.msExitFullscreen();
+   }
+}
